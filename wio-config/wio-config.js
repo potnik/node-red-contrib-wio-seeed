e2ea19cd87ef68be0cd994cdf763078cbee1609d
potnik <tkintop@gmail.com>
2016-12-18 19:35:45 -0600

    modified wio-config to add login server and fix token

--- wio-config/wio-config.js
+++ wio-config/wio-config.js
@@ -1,5 +1,5 @@
-﻿module.exports = function (RED) {
-    'use strict';
+module.exports = function (RED) {
+  'use strict';
   var url = require('url');
   var path = require('path');
   var commonPath = path.resolve(__dirname + '/../wio-common');
@@ -8,35 +8,40 @@
     RED.nodes.createNode(this, config);
     this.server = config.server;
     this.token = config.token;
- }
- RED.nodes.registerType('wio-config', WioConfig, {
-  credentials: {
-   server: { type: 'text' },
-   token: { type: 'text' }
   }
- });
 
- RED.httpAdmin.get('/wio-config/:id', function (req, res) {
-  res.send(RED.nodes.getCredentials(req.params.id));
- });
+  RED.nodes.registerType('wio-config', WioConfig, {
+    credentials: {
+      loginserver: { type: 'text'},
+      server: { type: 'text' },
+      token: { type: 'text' }
+    }
+  });
 
- RED.httpAdmin.post('/wio-config/:id/auth', function (req, res) {
-  var credentials = {};
+  RED.httpAdmin.get('/wio-config/:id', function (req, res) {
+    res.send(RED.nodes.getCredentials(req.params.id));
+  });
 
-  if (req.body.server) {
-   credentials.server = req.body.server;
+  RED.httpAdmin.post('/wio-config/:id/auth', function (req, res) {
+    var credentials = {};
 
-  if (req.body.token) {
-   credentials.token = req.body.token;
+    if (req.body.server) {
+      credentials.server = req.body.server;
+    }
+      if (req.body.loginserver) {
+        credentials.loginserver = req.body.loginserver;
+    }
+        if (req.body.token) {
+          credentials.token = req.body.token;
+    }
+          RED.nodes.addCredentials(req.params.id, credentials);
+        });
 
-  RED.nodes.addCredentials(req.params.id, credentials);
- });
+        RED.httpAdmin.get('/wio-common', function (req, res) {
+          res.sendFile(path.join(commonPath, 'wio-common.js'));
+        });
 
- RED.httpAdmin.get('/wio-common', function (req, res) {
-  res.sendFile(path.join(commonPath, 'wio-common.js'));
- });
-
- RED.httpAdmin.get('/wio-modules', function (req, res) {
-  res.sendFile(path.join(commonPath, 'wio-modules.json'));
- });
-}
+        RED.httpAdmin.get('/wio-modules', function (req, res) {
+          res.sendFile(path.join(commonPath, 'wio-modules.json'));
+        });
+      };
