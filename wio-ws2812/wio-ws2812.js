24447e5fc8d398fa88fc71bb2f458af3577e67b9
potnik <tkintop@gmail.com>
2016-12-23 22:47:36 -0600

    added additional REST calls

--- wio-ws2812/wio-ws2812.js
+++ wio-ws2812/wio-ws2812.js
@@ -1,51 +1,66 @@
 module.exports = function (RED) {
-	const https = require('https');
+  const https = require('https');
 
-	function WioWs2812(config) {
-		RED.nodes.createNode(this, config);
-		var node = this;
-		node.connection = RED.nodes.getCredentials(config.connection);
+  function WioWs2812(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    node.connection = RED.nodes.getCredentials(config.connection);
 
-		if (node.connection) {
-			this.on('input', function (msg) {
-				var hexColors = [];
-				var count = Math.min(((config.mode == 'auto') ? msg.payload : config.count), config.count);
-				var colors = config.colors.split(',');
+    if (node.connection) {
+      this.on('input', function (msg) {
+        var hexColors = [];
+        var count = Math.min(((config.mode == 'auto') ? msg.payload : config.count), config.count);
+        var colors = config.colors.split(',');
 
-				for (var i = 0; i < count; i++)
-					if (colors[i]) hexColors.push(colors[i]);
+        var payloadint = (parseInt(msg.payload) ? parseInt(msg.payload) : 0);
+        var request = '';
+        var rmethod = 'POST';
 
-				for (i = (hexColors.length - 1) ; i < config.count; i++)
-					hexColors.push('000000');
+        for (var i = 0; i < count; i++)
+          if (colors[i]) hexColors.push(colors[i]);
 
-				node.status({ fill: 'blue', shape: 'dot', text: 'requesting' });
-				var req = https.request({
-					hostname: node.connection.server,
-					port: 443,
-					path: '/v1/node/' + config.port.replace(/:/g, '') + '/segment/'
-						+ config.pos + '/' + hexColors.join('') + '?access_token=' + config.node,
-					method: 'POST'
-				}, function (res) {
-					msg.payload = '';
-					res.on('data', function (chunk) {
-						try { msg.payload = JSON.parse(chunk); }
-						catch (e) { node.warn('api error'); }
-						node.status({});
-						node.send(msg);
-					});
-				});
+        for (i = (hexColors.length - 1) ; i < config.count; i++)
+          hexColors.push('000000');
 
-				req.on('error', function (err) {
-					msg.payload = err.toString();
-					node.status({ fill: 'red', shape: 'ring', text: err.code });
-					node.send(msg);
-				});
+        if (config.mode === 'clear') {
+          request = 'clear/' + config.count + '/' + colors[1] + '/';
+          // ((payloadint === 0) ? parseInt(config.led) : payloadint);
+        } else if (config.mode === 'segment') {
+          request = 'segment/' + config.pos + '/' + hexColors.join('') + '/ ';
+        } else if (config.mode === 'rainbowon'){
+          request = 'start_rainbow_flow/' + parseInt(config.count ) + '/' + parseInt(config.brightness) + '/' + parseInt(config.speed) +'/';
+        } else if (config.mode === 'rainbowoff'){
+          request = 'stop_rainbow_flow/';
+        }
 
-				req.end();
-			});
-		} else {
-			node.status({ fill: 'red', shape: 'ring', text: 'missing connection' });
-		}
-	}
-	RED.nodes.registerType('wio-ws2812', WioWs2812);
-}
+        node.status({ fill: 'blue', shape: 'dot', text: 'requesting' });
+        var req = https.request({
+          hostname: node.connection.server,
+          port: 443,
+          path: '/v1/node/' + config.port.replace(/:/g, '') + '/' +
+          request + '?access_token=' + config.node,
+          method: rmethod
+        }, function (res) {
+          msg.payload = '';
+          res.on('data', function (chunk) {
+            try { msg.payload = JSON.parse(chunk); }
+            catch (e) { node.warn('api error'); }
+            node.status({});
+            node.send(msg);
+          });
+        });
+
+        req.on('error', function (err) {
+          msg.payload = err.toString();
+          node.status({ fill: 'red', shape: 'ring', text: err.code });
+          node.send(msg);
+        });
+
+        req.end();
+      });
+    } else {
+      node.status({ fill: 'red', shape: 'ring', text: 'missing connection' });
+    }
+  }
+  RED.nodes.registerType('wio-ws2812', WioWs2812);
+};
